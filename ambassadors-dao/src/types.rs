8955b0ea4ee47817639e202726a1998056516be1
gitshreevatsa <vastas2002@gmail.com>
2022-02-21 18:57:24 +0530

    Unriewed and complex

---
+++ ambassadors-dao/src/types.rs
@@ -0,0 +1,51 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::Base64VecU8;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{Balance, Gas};
+
+/// 1 yN to prevent access key fraud.
+pub const ONE_YOCTO_NEAR: Balance = 1;
+
+/// Gas for single ft_transfer call.
+pub const GAS_FOR_FT_TRANSFER: Gas = Gas(10_000_000_000_000);
+
+/// Configuration of the DAO.
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Clone, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Config {
+    /// Name of the DAO.
+    pub name: String,
+    /// Purpose of this DAO.
+    pub purpose: String,
+    /// Generic metadata. Can be used by specific UI to store additional data.
+    /// This is not used by anything in the contract.
+    pub metadata: Base64VecU8,
+}
+
+
+/// Set of possible action to take.
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub enum Action {
+    /// Action to add proposal. Used internally.
+    AddProposal,
+    /// Action to remove given proposal. Used for immediate deletion in special cases.
+    RemoveProposal,
+    /// Vote to approve given proposal or bounty.
+    VoteApprove,
+    /// Vote to reject given proposal or bounty.
+    VoteReject,
+    /// Vote to remove given proposal or bounty (because it's spam).
+    VoteRemove,
+    /// Finalize proposal, called when it's expired to return the funds
+    /// (or in the future can be used for early proposal closure).
+    Finalize,
+    /// Move a proposal to the hub to shift into another DAO.
+    MoveToHub,
+}
+
+impl Action {
+    pub fn to_policy_label(&self) -> String {
+        format!("{:?}", self)
+    }
+}
--- ambassadors-dao/src/views.rs
+++ ambassadors-dao/src/views.rs
@@ -1,9 +1,100 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+
+use std::cmp::min;
+
 use crate::*;
 
+/// This is format of output via JSON for the proposal.
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct ProposalOutput {
+    /// Id of the proposal.
+    pub id: u64,
+    #[serde(flatten)]
+    pub proposal: Proposal,
+}
+
+/// This is format of output via JSON for the bounty.
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct BountyOutput {
+    /// Id of the bounty.
+    pub id: u64,
+    #[serde(flatten)]
+    pub bounty: Bounty,
+}
+
 #[near_bindgen]
-impl Contract {
-    /// Returns the semver of the contract
-    pub fn version(&self) -> String {
-        env!("CARGO_PKG_VERSION").to_string()
+impl Contract{
+    pub fn get_config(&self) -> Config {
+        self.config.get().unwrap().clone()
+    }
+
+    /// Returns policy of this contract.
+    pub fn get_policy(&self) -> Policy {
+        self.policy.get().unwrap().to_policy().clone()
+    }
+    pub fn get_available_amount(&self) -> U128 {
+        U128(env::account_balance() - self.get_locked_storage_amount().0 - self.locked_amount)
+    }
+    pub fn get_last_proposal_id(&self) -> u64 {
+        self.last_proposal_id
+    }
+
+    /// Get proposals in paginated view.
+    pub fn get_proposals(&self, from_index: u64, limit: u64) -> Vec<ProposalOutput> {
+        (from_index..min(self.last_proposal_id, from_index + limit))
+            .filter_map(|id| {
+                self.proposals.get(&id).map(|proposal| ProposalOutput {
+                    id,
+                    proposal: proposal.into(),
+                })
+            })
+            .collect()
+    }
+
+    /// Get specific proposal.
+    pub fn get_proposal(&self, id: u64) -> ProposalOutput {
+        let proposal = self.proposals.get(&id).expect("ERR_NO_PROPOSAL");
+        ProposalOutput {
+            id,
+            proposal: proposal.into(),
+        }
+    }
+
+    /// Get given bounty by id.
+    pub fn get_bounty(&self, id: u64) -> BountyOutput {
+        let bounty = self.bounties.get(&id).expect("ERR_NO_BOUNTY");
+        BountyOutput {
+            id,
+            bounty: bounty.into(),
+        }
+    }
+
+    /// Get number of bounties.
+    pub fn get_last_bounty_id(&self) -> u64 {
+        self.last_bounty_id
+    }
+
+    /// Get `limit` of bounties from given index.
+    pub fn get_bounties(&self, from_index: u64, limit: u64) -> Vec<BountyOutput> {
+        (from_index..std::cmp::min(from_index + limit, self.last_bounty_id))
+            .filter_map(|id| {
+                self.bounties.get(&id).map(|bounty| BountyOutput {
+                    id,
+                    bounty: bounty.into(),
+                })
+            })
+            .collect()
+    }
+
+    /// Get bounty claims for given user.
+    pub fn get_bounty_claims(&self, account_id: AccountId) -> Vec<BountyClaim> {
+        self.bounty_claimers.get(&account_id).unwrap_or_default()
+    }
+
+    /// Returns number of claims per given bounty.
+    pub fn get_bounty_number_of_claims(&self, id: u64) -> u32 {
+        self.bounty_claims_count.get(&id).unwrap_or_default()
     }
 }
