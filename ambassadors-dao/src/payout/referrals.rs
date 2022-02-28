d27ec517b81b6715d86590bfcd2f5a2484fe2192
gitshreevatsa <vastas2002@gmail.com>
2022-02-28 22:03:34 +0530

    ref contract logic to be impl

--- ambassadors-dao/src/payout/referrals.rs
+++ ambassadors-dao/src/payout/referrals.rs
@@ -1,26 +1,37 @@
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
+use near_sdk::{env, near_bindgen};
+use std::collections::HashMap;
 
 use super::*;
 
-// create a random number and assign to campus ambassador
-pub fn generate_randon_no() -> String{
-    let rand_string: String = thread_rng()
-    .sample_iter(&Alphanumeric)
-    .take(30)
-    .map(char::from)
-    .collect();
+struct Refferal{
+     campusambassador : env::predecessor_account_id(),
+     ref_id : String,
+     connect = HashMap<campusambassador, ref_id>,
 }
 
-
-
+impl Refferal{
+// create a random number and assign to campus ambassador
+    pub fn generate_randon_no(&mut self) -> String{
+        let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+        self.connect.insert(campusambassador, rand_string);
+        return rand_string.to_string();
+    }
+}
 
 
 #[near_bindgen]
 impl Contract {
     pub fn check_random_number(&self, ref_id : generate_randon_no()) {
         // check the input ref id is associated with any campus ambassador
+
         // if yes, return the id of the campus ambassador
+
         //then tranfer tokens to the campus ambasador
     }
 
