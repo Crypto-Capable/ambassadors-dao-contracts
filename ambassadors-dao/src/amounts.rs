653a12fa846990787f6bf35e171e84873c9e5e4d
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-03-03 14:55:17 +0530

    feat: add bounty amount constants

--- ambassadors-dao/src/amounts.rs
+++ ambassadors-dao/src/amounts.rs
@@ -12,3 +12,17 @@ pub const ContentCoordinationAmount: Balance = 25 * types::ONE_NEAR / 10;
 
 /// 0.5 near tokens
 pub const CARegisterReferralAmount: Balance = 5 * types::ONE_NEAR / 10;
+
+/// 15 near tokens
+pub const HackathonFirstPlaceAmount: Balance = 15 * types::ONE_NEAR;
+/// 10 near tokens
+pub const HackathonSecondPlaceAmount: Balance = 10 * types::ONE_NEAR;
+/// 5 near tokens
+pub const HackathonThirdPlaceAmount: Balance = 5 * types::ONE_NEAR;
+
+/// 7.5 near tokens
+pub const MemeContestFirstPlaceAmount: Balance = 75 * types::ONE_NEAR / 10;
+/// 5 near tokens
+pub const MemeContestSecondPlaceAmount: Balance = 5 * types::ONE_NEAR;
+/// 2.5 near tokens
+pub const MemeContestThirdPlaceAmount: Balance = 25 * types::ONE_NEAR / 10;
