d3fd5f5d64da0548f727a9c4713a010e537bc6f8
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-03-24 00:50:54 +0530

    chore: remove factory

--- ambassadors-dao-factory/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-RUSTFLAGS='-C link-arg=-s' cargo +stable build --target wasm32-unknown-unknown --release
-
-cp ../target/wasm32-unknown-unknown/release/ambassadors_dao_factory.wasm ./res/
