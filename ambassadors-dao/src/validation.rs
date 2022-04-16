66c1608231e622d955d71c3bdf1d99a7c0a174a3
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-04-16 11:22:48 +0530

    fix: use proper error code

--- ambassadors-dao/src/validation.rs
+++ ambassadors-dao/src/validation.rs
@@ -3,5 +3,9 @@ use crate::*;
 pub fn assert_valid_resource_url(url: &str) {
     let url = url.trim();
     assert!(!url.is_empty(), "{}", error::ERR_INVALID_RESOURCE_URL);
-    assert!(url.starts_with("http://") || url.starts_with("https://"));
+    assert!(
+        url.starts_with("http://") || url.starts_with("https://"),
+        "{}",
+        error::ERR_INVALID_RESOURCE_URL
+    );
 }
